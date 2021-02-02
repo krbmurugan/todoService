72cf25fa15697a5209f45e0cba21ce8db488bf4d
P2812865 <P2812865@host.docker.internal>
2021-02-02 05:32:08 -0600

    Addl methods

--- todo/src/main/java/com/rs/todo/TodoHomeService.java
+++ todo/src/main/java/com/rs/todo/TodoHomeService.java
@@ -2,10 +2,14 @@ package com.rs.todo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class TodoHomeService {
 	private Logger LOG = LoggerFactory.getLogger(TodoHomeService.class);
 	
@@ -35,6 +39,25 @@ public class TodoHomeService {
 		LOG.info("inside welcome3..");
 		
 		return new WelcomeBean("Welcome message from Bean");
+	}
+	
+	@GetMapping("/withpathvar/{name1}/check")
+	public WelcomeBean getMsgWithPathVar(@PathVariable String name1) {
+		LOG.info("path var method..{}",name1);
+		String resp = String.format("Hello welcome, %s", name1);
+		
+		return new WelcomeBean(resp);
+		
+	}
+	
+	@GetMapping("/withqueryparam")
+	public WelcomeBean getMsgWithQueryParam(@RequestParam(value = "username") String name1) {
+		LOG.info("withqueryparam method..{}",name1);
+//		throw new RuntimeException("Error happened");
+		String resp = String.format("Hello welcome, %s", name1);
+		
+		return new WelcomeBean(resp);
+		
 	}
 
 }
